be0c34517d632b46adccfd75f2f151014374a070
ramos.william <ramos.william@mj.gov.br>
2022-03-22 16:58:52 -0300

    implementando metodo salvar

--- src/main/java/com/williamramos/cursoalgaworks/api/controller/CozinhaController.java
+++ src/main/java/com/williamramos/cursoalgaworks/api/controller/CozinhaController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -48,6 +45,17 @@ public class CozinhaController {
             return ResponseEntity.status(HttpStatus.OK).body(obj);
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Cozinha> salvar(@RequestBody Cozinha cozinha){
+          Cozinha obj = repository.salvar(cozinha);
+          if(obj != null){
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(obj);
+          }
+          return ResponseEntity.internalServerError().build();
 
     }
 
